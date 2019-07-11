78acb54cc48b8966912df03af1e20daed84165a4
DMoreno06 <mordiana1506@gmail.com>
2019-07-11 15:45:38 -0500

    tipos de datos

---
+++ moduleFour/app.js
@@ -0,0 +1,25 @@
+//variables cadena
+let nombre = 'diana';
+let apellido= 'Moreno';
+
+
+let nombreCompleto = nombre+' '+apellido;
+
+console.log('tu nombre es '+ nombreCompleto);
+//variables numericas
+let numero = '3';
+let numeroDos = '7';
+
+console.log(numero + numeroDos);
+
+let a = 7;
+let b = 9;
+
+console.log(a+b);
+
+//variables buleanas
+
+let activo = true;
+let inactivo = false;
+
+console.log(activo+inactivo);
